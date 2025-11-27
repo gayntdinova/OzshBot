506e9a6ec22df654c5a9da1efb71fb64e67b1bf5
Irina Gayntdinova <gajntdinovairina@gmail.com>
2025-11-27 17:31:54 +0500

    первая версия дописанного FindService +RoleService

--- OzshBot/OzshBot.Application/Services/UserFindService.cs
+++ OzshBot/OzshBot.Application/Services/UserFindService.cs
@@ -30,9 +30,50 @@ public class UserFindService: IUserFindService
             : Result.Ok(users);
     }
 
-    public Task<Result<User[]>> FindUserAsync(string target)
+    public async Task<Result<User[]>> FindUserAsync(string target)
     {
-        throw new NotImplementedException();
+        var splitedTarget = target.Split(" ");
+        if (splitedTarget.Length == 1)
+        {
+            target = target.Replace("@", "");
+            var userByTg = await FindUserByTgAsync(new TelegramInfo() { TgId = null, TgUsername = target });
+            if (userByTg.IsSuccess) return Result.Ok(new []{userByTg.Value});
+        }
+
+        var usersByTown = await FindUsersByTownAsync(target);
+        if (usersByTown.IsSuccess) return Result.Ok(usersByTown.Value);
+        var combinations = MakeCombinationsFIOBystring(splitedTarget);
+        foreach (var combination in combinations)
+        {
+            var userByFullName = await FindUsersByFullNameAsync(null);
+            if (userByFullName.IsSuccess) return Result.Ok(userByFullName.Value);
+        }
+        return Result.Fail($"users with {target} was not found");
+    }
+
+    private List<FullName> MakeCombinationsFIOBystring(string[] splitedTarget)
+    {
+        var fullNameCombinations = new List<FullName>();
+        if (splitedTarget.Length == 1)
+        {
+            fullNameCombinations.Add(new FullName(name: splitedTarget[0]));
+            fullNameCombinations.Add(new FullName(surname: splitedTarget[0]));
+            fullNameCombinations.Add(new FullName(patronymic: splitedTarget[0]));
+        }
+
+        if (splitedTarget.Length == 2)
+        {
+            fullNameCombinations.Add(new FullName(name: splitedTarget[0], surname: splitedTarget[1]));
+            fullNameCombinations.Add(new FullName(name: splitedTarget[1], surname: splitedTarget[0]));
+            fullNameCombinations.Add(new FullName(name: splitedTarget[0], patronymic: splitedTarget[1]));
+        }
+
+        if (splitedTarget.Length == 3)
+        {
+            fullNameCombinations.Add(new FullName(name: splitedTarget[1], surname: splitedTarget[0], patronymic: splitedTarget[2]));
+        }
+
+        return fullNameCombinations;
     }
     
     private async Task<Result<User>> FindUserByTgAsync(TelegramInfo telegramInfo)
@@ -47,7 +88,7 @@ public class UserFindService: IUserFindService
     {
         var users = await userRepository.GetUsersByFullNameAsync(fullName);
         return users == null
-            ? Result.Fail($"users with {} was not found")//todo check what exactly was completed in the fullname
+            ? Result.Fail($"users with {fullName.ToString()} was not found")
             : Result.Ok(users);
     }
 
--- OzshBot/OzshBot.Domain/ValueObjects/FullName.cs
+++ OzshBot/OzshBot.Domain/ValueObjects/FullName.cs
@@ -2,7 +2,23 @@ namespace OzshBot.Domain.ValueObjects;
 
 public class FullName
 {
-    public required string? Name { get; set; }
-    public required string? Surname { get; set; }
-    public required string? Patronymic { get; set; }
+    public FullName(string? name=null, string? surname=null, string? patronymic=null)
+    {
+        Name = name;
+        Surname = surname;
+        Patronymic = patronymic;
+    }
+
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public string? Patronymic { get; set; }
+
+    public override string ToString()
+    {
+        var resultString = string.Empty;
+        if (Name != null) resultString += $" {Name}";
+        if (Surname != null) resultString += $" {Surname}";
+        if (Patronymic != null) resultString += $" {Patronymic}";
+        return resultString;
+    }
 }
