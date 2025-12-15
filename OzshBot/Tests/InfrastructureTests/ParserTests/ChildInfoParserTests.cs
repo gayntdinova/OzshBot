e3b77fbf61bdf60da2a157ba485d7f232f34f214
Валерия Москвина <v.y.moskvina@gmail.com>
2025-12-15 17:59:30 +0500

    ещё немного тестов

--- OzshBot/Tests/InfrastructureTests/ParserTests/ChildInfoParserTests.cs
+++ OzshBot/Tests/InfrastructureTests/ParserTests/ChildInfoParserTests.cs
@@ -15,16 +15,21 @@ public class ChildInfoParserTests
         Assert.Pass();
     }
 
-    [Test]
-    public void ChildInfoParser_Works_WithIncorrectColumnIndexes()
+    [TestCaseSource(nameof(CorrectColumnIndexesTestCases))]
+    public void ChildInfoParser_Works_WithCorrectColumnIndexes(List<string> data, Dictionary<string, int> dict, ChildDto expected)
     {
-        Assert.Pass();
+        var actual = new ChildInfoParser(dict).CreateChildDto(data);
+        Assert.That(actual.City, Is.EqualTo(expected.City));
+        Assert.That(actual.Birthday, Is.EqualTo(expected.Birthday));
+        Assert.That(actual.PhoneNumber, Is.EqualTo(expected.PhoneNumber));
+        Assert.That(actual.Email,Is.EqualTo(expected.Email));
+        Assert.That(actual.ChildInfo.EducationInfo, Is.EqualTo(expected.ChildInfo.EducationInfo));
+        //Assert.That(actual, Is.EqualTo(expected));
     }
 
-    [Test]
-    public void ChildInfoParser_Fails_WithIncorrectColumnIndexes()
+    [TestCaseSource(nameof(IncorrectColumnIndexesTestCases)) ]
+    public void ChildInfoParser_Fails_WithIncorrectColumnIndexes(Dictionary<string, int> dict)
     {
-        var dict = new Dictionary<string, int>();
         Assert.That(() => new ChildInfoParser(dict), Throws.InvalidOperationException);
     }
     
@@ -33,7 +38,7 @@ public class ChildInfoParserTests
         new[]
         {
             new TestCaseData(new Dictionary<string, int>()),
-            new TestCaseData(new Dictionary<string, int?>()
+            new TestCaseData(new Dictionary<string, int>()
             {
                 { "фио", -1 },
             })
@@ -44,12 +49,12 @@ public class ChildInfoParserTests
         var childDto = new ChildDto
         {
             FullName = new FullName("Иванов", "Иван", "Иванович"),
-            Birthday = new DateOnly(2000,1, 1),
+            Birthday = new DateOnly(2001,1, 1),
             ChildInfo = new ChildInfo
             {
                 EducationInfo = new EducationInfo(){ Class = 6, School = "school 1"}
             },
-            City = "Muhosransk",
+            City = "екатеринбург",
             Email = "child@child.com",
             PhoneNumber = "+71111111111"
         };
@@ -58,20 +63,29 @@ public class ChildInfoParserTests
             new TestCaseData(
                 new List<string>()
             {
-                
+                "Иванов Иван Иванович",
+                "6",
+                "екатеринбург",
+                "school 1",
+                "01.01.2001",
+                "+71111111111",
+                "child@child.com",
+                "",
+                ""
             }, 
                 new Dictionary<string, int>()
             {
-                {"фио", 1},
-                {"класс", 2},
-                {"город", 3},
-                {"школа", 4}, 
-                {"день рождения", 5},
-                {"телефон", 6},
-                {"email",  7},
-                {"комментарий", 8},
-                {"статус заявки на сайте", 9}
-            })
+                {"фио", 0},
+                {"класс", 1},
+                {"город", 2},
+                {"школа", 3}, 
+                {"день рождения", 4},
+                {"телефон", 5},
+                {"email",  6},
+                {"комментарий", 7},
+                {"статус заявки на сайте", 8}
+            }, 
+                childDto)
         };
     }
     
