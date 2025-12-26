06e58b8b298552338556fedfd4915f4d37751788
Andrew Goodman <goodsoul.andrew@mail.ru>
2025-12-26 19:56:33 +0500

    поправил Capitalize

--- OzshBot/OzshBot.Domain/ValueObjects/FullName.cs
+++ OzshBot/OzshBot.Domain/ValueObjects/FullName.cs
@@ -6,14 +6,17 @@ public record FullName
     public string Name { get; private init; }
     public string? Patronymic { get; private init; }
 
-    public FullName(string surname, string name,  string? patronymic=null)
+    public FullName(string surname, string name, string? patronymic = null)
     {
         Name = Capitalize(name);
         Surname = Capitalize(surname);
         Patronymic = patronymic != null ? Capitalize(patronymic) : null;
     }
-        private static string Capitalize(string str)
+
+    private static string Capitalize(string str)
     {
+        if (string.IsNullOrEmpty(str)) return str;
+        if (str.Length == 1) return str.ToUpper();
         return str[..1].ToUpper() + str[1..].ToLower();
     }
 }
